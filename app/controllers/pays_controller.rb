73f2071940082b647221a77bc6cd466384c9e591
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2023-03-12 23:45:37 +0900

    payjpのテスト機能作成

--- app/controllers/pays_controller.rb
+++ app/controllers/pays_controller.rb
@@ -1,4 +1,10 @@
 class PaysController < ApplicationController
   def index
   end
+
+  def create
+    Payjp.api_key = ENV['PAYJP_TEST_SECRET_KEY']
+    # charge = Payjp::Charge.create(:amount => 3500,:card => params[:token], :currency => 'jpy')
+    byebug
+  end
 end
